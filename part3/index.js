2de155997afb67757089a057417d29f09933f9d8
ezequielzion <ezequielzion@gmail.com>
2022-09-29 03:41:55 -0300

    Part 3 WIP

--- part3/index.js
+++ part3/index.js
@@ -1,6 +1,7 @@
  const express = require('express')
 
 const app = express()
+app.use(express.json())
 
 let notes = [
   {
@@ -33,6 +34,29 @@ app.get('/', (request, response) => {
 
 app.get('/api/notes', (request, response) => {
   response.json(notes)
+})
+
+app.get('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const note = notes.find(note => note.id === id)
+  if (note) {
+    response.json(note)
+  } else {
+    response.status(404).end()
+  }
+})
+
+app.delete('/api/notes/:id', (request, response) => {
+  const id = Number(request.params.id)
+  notes = notes.filter(note => note.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/notes', (request, response) => {
+  const note = request.body
+  console.log(note)
+  response.json(note)
 })
 
 const PORT = 3001
