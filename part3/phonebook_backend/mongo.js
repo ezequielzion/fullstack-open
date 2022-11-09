8e31cd9655c8ce577037efa85abe51f441d6d7b3
ezequielzion <ezequielzion@gmail.com>
2022-11-09 03:11:55 -0300

    Finished exercise 3.12

---
+++ part3/phonebook_backend/mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+if (process.argv.length < 3) {
+  console.log('Please provide the password as an argument: node mongo.js <password>')
+  process.exit(1)
+}
+
+const password = process.argv[2]
+const name = process.argv[3]
+const number = process.argv[4]
+
+const url = `mongodb+srv://ezion-fullstackopen:${password}@phonebook.azjw1q5.mongodb.net/phonebook?retryWrites=true&w=majority`
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  number: String,
+})
+
+const Person = mongoose.model('Person', personSchema)
+
+mongoose
+  .connect(url)
+  .then((result) => {
+    console.log('connected')
+    
+    if (name && number) {
+      const person = new Person({
+        name: name,
+        number: number
+      })
+      return person.save()
+      .then(() => {
+        console.log(`added ${name} number ${number} to phonebook`);
+        return mongoose.connection.close()
+      })
+    } else {
+      Person
+        .find({})
+        .then(result => {
+          console.log("Phonebook:");
+          result.forEach(person => console.log(person.name, person.number))
+          mongoose.connection.close()
+        })
+    }
+  })
+  .catch((err) => console.log(err))
