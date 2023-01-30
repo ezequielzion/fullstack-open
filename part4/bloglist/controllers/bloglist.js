962a50f3eded8df4a3af85393acc5e910b13e505
ezequielzion <ezequielzion@gmail.com>
2023-01-30 03:49:23 -0300

    Part 4: WIP exercise 4.17

--- part4/bloglist/controllers/bloglist.js
+++ part4/bloglist/controllers/bloglist.js
@@ -1,17 +1,31 @@
 const bloglistRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 bloglistRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
+
   response.json(blogs)
 })
 
 bloglistRouter.post('/', async (request, response) => {
-  const blogObj = request.body
-  if(!blogObj.title || !blogObj.url) response.status(400).end()
-  
-  if(!blogObj.likes) blogObj.likes = 0
-  const blog = new Blog(blogObj)
+  const {title, author, url, likes} = request.body
+
+  if(!title || !url) response.status(400).end()
+  if(!likes) likes = 0
+
+  const users = await User.find({})
+  const index = Math.floor(Math.random() * users.length)
+  const user = users[index]
+
+  const blog = new Blog({
+    title: title,
+    author: author,
+    url: url,
+    likes: likes,
+    user: user._id
+  })
+
   const savedBlog = await blog.save()
   response.status(201).json(savedBlog)
 })
