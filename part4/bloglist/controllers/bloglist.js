b0f69e7044e3e24e3b924e2a73c6fbf72c7c22fd
ezequielzion <ezequielzion@gmail.com>
2023-02-19 04:23:38 -0300

    part5: updated backend for exercise 5.9

--- part4/bloglist/controllers/bloglist.js
+++ part4/bloglist/controllers/bloglist.js
@@ -60,7 +60,8 @@ bloglistRouter.put('/:id', async (request, response) => {
     title: body.title,
     author: body.author,
     url: body.url,
-    likes: body.likes
+    likes: body.likes,
+    user: body.user
   }
 
   const updatedBlog =  await Blog.findByIdAndUpdate(request.params.id, blog, { new: true })
