2eb89cff5e63cdd763b299ce79e0e38a2ebbc3fa
ezequielzion <ezequielzion@gmail.com>
2023-03-17 02:48:42 -0300

    part5: exercise 5.14 done

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
+import userEvent from '@testing-library/user-event'
 import { render, screen } from '@testing-library/react'
 import Blog from './Blog'
 
@@ -33,4 +34,43 @@ test('renders content', () => {
 
   const url = container.querySelector('#url')
   expect(url).toBeNull()
+})
+
+test('clicking the button calls event handler once', async () => {
+  const blog = {
+    title: 'This is the title',
+    author: 'John Johnson',
+    likes: 100,
+    url: 'www.medium.com',
+    user: { name: 'pepo', username: 'pepe' }
+  }
+
+  const mockHandler = jest.fn()
+
+  render(
+    <Blog
+      blog={blog}
+      addLike={mockHandler}
+      deleteBlog={mockHandler}
+      user={{ name: 'pepo', username: 'pepe' }}
+    />
+  )
+
+  const user = userEvent.setup()
+  const button = screen.getByText('Show')
+  expect(button).toBeDefined()
+  await user.click(button)
+
+  const title = screen.getByText('This is the title')
+  expect(title).toBeDefined()
+
+  const author = screen.getByText('John Johnson')
+  expect(author).toBeDefined()
+
+  const url = screen.getByText('www.medium.com')
+  expect(url).toBeDefined()
+
+  const likes = screen.getByText(100)
+  expect(likes).toBeDefined()
+
 })
