2dcaee88dded486cddd319c4b24e02067fa02585
ezequielzion <ezequielzion@gmail.com>
2023-03-27 03:14:01 -0300

    extra (part5): added more Cypress tests

--- extra/part5-notes-frontend/cypress/e2e/spec.cy.js
+++ extra/part5-notes-frontend/cypress/e2e/spec.cy.js
@@ -1,7 +1,23 @@
 describe('Note app', function() {
-  it('front page can be opened', function() {
+  beforeEach(function() {
     cy.visit('http://localhost:3000')
+  })
+
+  it('front page can be opened', function() {
     cy.contains('Notes')
     cy.contains('CHIMBA')
+  })
+
+  it('login form can be opened', function() {
+    cy.contains('login').click()
+  })
+
+  it('user can login', function () {
+    cy.contains('login').click()
+    cy.get('#username').type('mluukkai')
+    cy.get('#password').type('salainen')
+    cy.get('#login-button').click()
+
+    cy.contains('Matti logged in')
   })
 })
