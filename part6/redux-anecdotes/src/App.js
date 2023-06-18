3847b623f6c1c2c834ab8d9d2da9ca634df47c80
ezequielzion <ezequielzion@gmail.com>
2023-06-18 03:59:33 -0300

    part6: started ex. 6.3

---
+++ part6/redux-anecdotes/src/App.js
@@ -0,0 +1,49 @@
+import { useSelector, useDispatch } from 'react-redux'
+
+const App = () => {
+  const anecdotes = useSelector(state => state)
+  const dispatch = useDispatch()
+
+  const vote = (id) => {
+    console.log('vote', id)
+  }
+
+  const addAnecdote = (event) => {
+      event.preventDefault()
+      const content = event.target.anecdote.value
+      event.target.anecdote.value = ''
+      store.dispatch({
+        type: 'NEW_NOTE',
+        payload: {
+          content,
+          important: false,
+          id: generateId()
+        }
+      })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Anecdotes</h2>
+      {anecdotes.map(anecdote =>
+        <div key={anecdote.id}>
+          <div>
+            {anecdote.content}
+          </div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      )}
+      <h2>create new</h2>
+      <form onSubmit={addAnecdote}>
+        <div><input name="anecdote"/></div>
+        <button  type="submit">create</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
